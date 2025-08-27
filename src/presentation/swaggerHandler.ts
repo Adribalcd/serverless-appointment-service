23b9c153d487639557448f11d3a08481c929935a
Adriana Balceda <adribalcd@gmail.com>
2025-08-27 13:20:51 -0500

    docs: include swagger.yml in package and improve API documentation

--- src/presentation/swaggerHandler.ts
+++ src/presentation/swaggerHandler.ts
@@ -2,18 +2,38 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as yaml from 'js-yaml';
+import * as swaggerUi from 'swagger-ui-dist';
 
 export const swaggerDocs: APIGatewayProxyHandler = async () => {
   try {
-    const filePath = path.resolve(process.cwd(), 'swagger.yml');
-
+    const filePath = path.resolve(process.cwd(), 'docs/swagger.yml');
     const swaggerYaml = fs.readFileSync(filePath, 'utf8');
     const swaggerJson = yaml.load(swaggerYaml);
 
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>API Docs</title>
+          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+        </head>
+        <body>
+          <div id="swagger-ui"></div>
+          <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+          <script>
+            SwaggerUIBundle({
+              spec: ${JSON.stringify(swaggerJson)},
+              dom_id: '#swagger-ui',
+            });
+          </script>
+        </body>
+      </html>
+    `;
+
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(swaggerJson, null, 2),
+      headers: { 'Content-Type': 'text/html' },
+      body: html,
     };
   } catch (err: any) {
     return {
